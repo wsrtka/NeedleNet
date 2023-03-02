77a06dea67fb97077fb59eeee145bc6e0ae0e270
wsrtka <witold.serwatka@pm.me>
2023-03-02 10:44:45 +0100

    add average module

--- needlenet/model.py
+++ needlenet/model.py
@@ -1,1 +1,21 @@
 """Module containing audio classification model."""
+# pylint: disable=import-error
+
+import torch
+
+from torch import nn
+
+
+# pylint: disable=too-few-public-methods, invalid-name
+class Average(nn.Module):
+    """Module to get average of elements along specified axis."""
+
+    def __init__(self, axis):
+        super().__init__()
+        self.axis = axis
+
+    def forward(self, x):
+        """Average input along specified axis."""
+        output = x.mean(dim=self.axis)
+        output = torch.squeeze(output, self.axis)
+        return output
