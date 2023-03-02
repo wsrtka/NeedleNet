c402bb7622ce76fe0ec03e95d61071e9392e4fc4
wsrtka <witold.serwatka@pm.me>
2023-03-02 11:39:31 +0100

    add training function

--- needlenet/engine.py
+++ needlenet/engine.py
@@ -1,5 +1,43 @@
 """File containing training functions."""
+# pylint: disable=import-error
+
+import torch
+
+from tqdm.auto import tqdm
 
 
-def train_model():
+# pylint: disable=invalid-name,too-many-arguments,too-many-locals
+def train_model(model, epochs, loss_fn, acc_fn, train_dl, test_dl, optimizer):
     """Function to train model."""
+    for epoch in tqdm(range(epochs)):
+        print(f"Epoch #{epoch}/{epochs}")
+        train_loss = 0
+
+        for batch, (X, y) in enumerate(train_dl):
+            y_pred = model(X)
+            loss = loss_fn(y_pred, y)
+            train_loss += loss
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            if batch % 10 == 0:
+                print(f"{batch * len(X) / len(train_dl.dataset)} samples, loss: {loss}")
+
+        train_loss /= len(train_dl)
+        test_loss = 0
+        test_acc = 0
+
+        model.eval()
+        with torch.inference_mode():
+            for X, y in test_dl:
+                test_pred = model(X)
+                test_loss += loss_fn(test_pred, y)
+                test_acc += acc_fn(y_true=y, y_pred=test_pred.argmax(dim=1))
+
+            test_loss /= len(test_dl)
+            test_acc /= len(test_dl)
+
+        print(
+            f"Train loss: {train_loss:.5f} | Test loss: {test_loss:.5f}, Test acc: {test_acc:.2f}%"
+        )
